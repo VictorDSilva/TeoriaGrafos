c13a7d5deec79c83a28ae10edff3466f401a25d0
Victor Domingos <victordomingos91@gmail.com>
2018-01-16 09:25:12 -0200

    Vizinho mais proximo

--- src/tsp/PrincipalTSP.java
+++ src/tsp/PrincipalTSP.java
@@ -10,32 +10,32 @@ import java.util.Scanner;
 public class PrincipalTSP {
 
     public static void main(String[] arg) {
-        int number_of_nodes;
+        int numeroNodes;
         Scanner scanner = null;
         try {
             System.out.print("Digite com Numero de VERTICES do Grafo: ");
             scanner = new Scanner(System.in);
-            number_of_nodes = scanner.nextInt();
+            numeroNodes = scanner.nextInt();
 
-            int adjacency_matrix[][] = new int[number_of_nodes + 1][number_of_nodes + 1];
+            int matrizAdj[][] = new int[numeroNodes + 1][numeroNodes + 1];
             System.out.println("Entre com a Matriz de Adjacencia");
 
-            for (int i = 1; i <= number_of_nodes; i++) {
-                for (int j = 1; j <= number_of_nodes; j++) {
-                    adjacency_matrix[i][j] = scanner.nextInt();
+            for (int i = 1; i <= numeroNodes; i++) {
+                for (int j = 1; j <= numeroNodes; j++) {
+                    matrizAdj[i][j] = scanner.nextInt();
                 }
             }
-            for (int i = 1; i <= number_of_nodes; i++) {
-                for (int j = 1; j <= number_of_nodes; j++) {
-                    if (adjacency_matrix[i][j] == 1 && adjacency_matrix[j][i] == 0) {
-                        adjacency_matrix[j][i] = 1;
+            for (int i = 1; i <= numeroNodes; i++) {
+                for (int j = 1; j <= numeroNodes; j++) {
+                    if (matrizAdj[i][j] == 1 && matrizAdj[j][i] == 0) {
+                        matrizAdj[j][i] = 1;
                     }
                 }
             }
 
             System.out.println("\nA ordem para visita será: ");
             VizinhoMaisProximo algoritmoTSP = new VizinhoMaisProximo();
-            algoritmoTSP.tsp(adjacency_matrix);
+            algoritmoTSP.tsp(matrizAdj);
             
         } catch (InputMismatchException inputMismatch) {
             System.out.println("Formato Inválido!");
